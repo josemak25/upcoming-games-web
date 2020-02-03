6d0fb4cb1fac02b49af0e73b4801d1f5ed49d8df
josemak25 <amakirij@gmail.com>
2020-02-03 18:27:33 +0100

    add home styles

---
+++ src/pages/home/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { colors } from "../../constants";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.BUTTON_COLOR};
+`;
+
+export const Logo = styled.img`
+  width: 10%;
+  height: 10%;
+`;
