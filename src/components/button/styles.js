41f2ca791a5c1a30d222a545ab3c968fc3ef21c5
josemak25 <amakirij@gmail.com>
2020-02-03 15:24:02 +0100

    add initial setup for app

---
+++ src/components/button/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../../constants";
+
+export const Container = styled.button`
+  min-width: 50px;
+  min-height: 100px;
+  background-color: ${colors.BUTTON_COLOR};
+`;
