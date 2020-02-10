506782433ece2675a340be994d43f60171f3f326
fredele <victorisholaoladele@gmail.com>
2020-02-10 10:14:15 +0100

    Added some jsx to check if styles worked properly

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,22 +1,77 @@
-import React from 'react'
-import { Container, Item, Card, Title } from "./styles" 
+import React from "react";
+import { Container, Card, Title } from "./styles";
 
-export default function index() {
-    return (
-        <Container>
-            <Item>
-                <Card>
-                    <img src="./img/johnwick.jfif" alt="" />
-                    <h5>2017</h5>
-                    <Title>
-                        <h4>John Wick: Chapter 2</h4>
-                        <h6>action, triller</h6>
-                        <p>4.8</p>
-                    </Title>
-                </Card>
-            </Item>
-        </Container>
-    )
+export default function GameCard({ year, title, genres, rating, image }) {
+  return (
+    <Container>
+      <Card>
+        <img src={image} alt="" />
+        <h5>{year}</h5>
+        <Title>
+          <div className="flex-item">
+            <h4>{`${title.substring(0, 18)}...`}</h4>
+            <h6>{genres}</h6>
+          </div>
+          <p className="flex-item">{rating}</p>
+        </Title>
+      </Card>
+
+      <Card>
+        <img src={image} alt="" />
+        <h5>{year}</h5>
+        <Title>
+          <div className="flex-item">
+            <h4>{`${title.substring(0, 18)}...`}</h4>
+            <h6>{genres}</h6>
+          </div>
+          <p className="flex-item">{rating}</p>
+        </Title>
+      </Card>
+
+      <Card>
+        <img src={image} alt="" />
+        <h5>{year}</h5>
+        <Title>
+          <div className="flex-item">
+            <h4>{`${title.substring(0, 18)}...`}</h4>
+            <h6>{genres}</h6>
+          </div>
+          <p className="flex-item">{rating}</p>
+        </Title>
+      </Card>
+
+      <Card>
+        <img src={image} alt="" />
+        <h5>{year}</h5>
+        <Title>
+          <div className="flex-item">
+            <h4>{`${title.substring(0, 18)}...`}</h4>
+            <h6>{genres}</h6>
+          </div>
+          <p className="flex-item">{rating}</p>
+        </Title>
+      </Card>
+
+      <Card>
+        <img src={image} alt="" />
+        <h5>{year}</h5>
+        <Title>
+          <div className="flex-item">
+            <h4>{`${title.substring(0, 18)}...`}</h4>
+            <h6>{genres}</h6>
+          </div>
+          <p className="flex-item">{rating}</p>
+        </Title>
+      </Card>
+    </Container>
+  );
 }
 
-
+GameCard.defaultProps = {
+  image:
+    "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2Fa7c0a756d6b04801bd97d880c40e4113%2F960x0.jpg%3Ffit%3Dscale",
+  year: 2017,
+  title: "John Wick: Chapter 2",
+  genres: "action, triller",
+  rating: 4.5
+};
