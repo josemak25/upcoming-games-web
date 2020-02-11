17e969f230607719984ca2253adb03aaa45b4729
fredele <victorisholaoladele@gmail.com>
2020-02-11 11:24:58 +0100

    Added some style for responsive view

--- src/components/ratings/styles.js
+++ src/components/ratings/styles.js
@@ -3,7 +3,7 @@ import {colors} from '../../constants/index'
 
 export const Container = styled.div`
     background: #282829;
-    width: 18%;
+    width: 18% !important;
     height: 80px;
     margin: auto;
     border-radius: 2px;
@@ -25,6 +25,49 @@ export const Container = styled.div`
     div > h3 > span {
         font-size: 15px;
         color: ${colors.FONT_DARK_COLOR}
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px){
+        width: 35% !important;
+        height: 100px;
+
+        div {
+            margin-left: 10px;
+        }
+
+        div > h3 {
+            font-size: 25px;
+        }
+
+        div h3 span {
+            font-size: 20px;
+        }
+    }
+
+    @media screen and (max-width: 768px){
+        width: 45% !important;
+    }
+
+    @media only screen and (max-width: 500px) {
+        width: 50% !important;
+        height: 60px;
+
+        div > h3 {
+            font-size: 13px;
+            margin-top: 4px;
+        }
+        div > h3 > span {
+            font-size: 11px;
+        }
+
+    }
+
+    @media screen and (max-width: 320px) {
+        width: 58% !important;
+    }
+
+    @media screen and (min-width: 411px) {
+        width: 45%;
     }
 
 
@@ -51,4 +94,31 @@ export const MainStar = styled.div`
             colors.FONT_DARK_COLOR
         };
     }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+        .star {
+            padding-right: 15px;
+            font-size: 25px; 
+            margin-left: 10px;
+            margin-top: 5px;
+        }
+
+        div {
+            font-size: 30px;
+            margin-top: -5px;
+        }
+    }
+
+    @media only screen and (max-width: 500px) {
+        .star {
+            font-size: 15px;
+            padding-right: 10px;
+        }
+
+        div {
+            margin-top: -5px;
+        }
+    }
+
+    
 `
